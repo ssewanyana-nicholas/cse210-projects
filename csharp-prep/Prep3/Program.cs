6bc21916a7b3300017fb8a1b57de412379381269
ssewanyana-nicholas <nicssewanyana@gmail.com>
2023-09-19 09:54:52 +0300

    completed the conditional statement

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int attempts = 0;
+
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string think = Console.ReadLine();
+            guess = int.Parse(think);
+
+            attempts++;
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("Congratulations! You have guessed it");
+                Console.WriteLine($"You have used {attempts} guesses");
+            }
+        }
     }
 }
